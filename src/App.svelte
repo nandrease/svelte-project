210fbbb104dea548c56908dfefa34fea4628071b
nandrease <neeme.andreas.eller@gmail.com>
2020-03-12 17:12:16 +0200

    Add MeetupItem

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,40 @@
 <script>
+  import Header from "./UI/Header.svelte";
+  import MeetupItem from "./Meetups/MeetupItem.svelte";
 
+  const meetups = [
+    {
+      id: "m1",
+      title: "Coding Bootcamp",
+      subtitle: "Learn to code in 2 hours",
+      description:
+        "I this meetup we will learn to code with Svelte - a lightweight JavaScript compiler",
+      imageUrl: "https://source.unsplash.com/random",
+      address: "Akadeemia tee 15A, TAllinn, Estonia",
+      contactEmail: "na@an.com"
+    },
+    {
+      id: "m2",
+      title: "Ski Trip to Are",
+      subtitle: "One week in Sweden in March",
+      description:
+        "I this meetup we will learn to code with Svelte - a lightweight JavaScript compiler",
+      imageUrl: "https://source.unsplash.com/random",
+      address: "Trondheimsleden 52, 837 52 Åre, Sweden",
+      contactEmail: "na@an.com"
+    }
+  ];
 </script>
 
 <style>
-
+  #meetups {
+    margin-top: 5rem;
+  }
 </style>
 
-<h1>Time for a project!</h1>
+<Header />
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetupItem {meetup} />
+  {/each}
+</section>
